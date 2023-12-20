47cd515397f66bbd6f6fd0f67195e1e8894e40bf
mehmet_yasar <mehmet.yasar0770@gmail.com>
2023-12-20 22:59:08 +0300

    Tabloda arama tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import { useState } from 'react';
 import './App.css';
 import { Users } from './users.js';
+import Table from './Table.jsx';
 
 function App() {
 
   const [query,setQuery] = useState("");
 
+  const keys = ["first_name", "last_name","email"]
+
+
+  const search = (data) => {
+    return data.filter ((item) => 
+    keys.some((key)=> item[key].toLowerCase().includes(query))
+    )
+  }
+
   
   return (
     <div className="App">
       <input type='text' className='search' placeholder='Search...' onChange={(e) => setQuery(e.target.value)}></input>
-      <ul className='list'>
+     
+     
+     {/*} <ul className='list'>
 
-        {Users.filter(user => user.first_name.toLowerCase().includes(query)) .map((user)=> (
+       {Users.filter(user => user.first_name.toLowerCase().includes(query)) .map((user)=> (
            <li key={user.id} className='listItem'> {user.first_name}</li>
         ))}
 
        
        
-      </ul>
-
+       </ul>  {*/}
+      <Table data={search(Users)}/>
       
     </div>
   );
